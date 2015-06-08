2fbf53145be804f3093838a31506b3434f835643
Simon Mourier <gun.dark@noos.fr>
2015-06-08 21:34:27 +0200

    Added missing WpfExtensions.cs file

---
+++ WPFHtmlEditor/WpfExtensions.cs
@@ -0,0 +1,177 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace SoftFluent.Tools
+{
+    public static class WpfExtensions
+    {
+        public static void SetTracing()
+        {
+            SetTracing(SourceLevels.Warning, null);
+        }
+
+        public static void SetTracing(SourceLevels levels, TraceListener listener)
+        {
+            if (listener == null)
+            {
+                listener = new DefaultTraceListener();
+            }
+
+            PresentationTraceSources.Refresh();
+            foreach (PropertyInfo pi in typeof(PresentationTraceSources).GetProperties(BindingFlags.Static | BindingFlags.Public))
+            {
+                if (pi.Name == "FreezableSource")
+                    continue;
+
+                if (typeof(TraceSource).IsAssignableFrom(pi.PropertyType))
+                {
+                    TraceSource ts = (TraceSource)pi.GetValue(null, null);
+                    ts.Listeners.Add(listener);
+                    ts.Switch.Level = levels;
+                }
+            }
+        }
+
+        public static IEnumerable<DependencyObject> EnumerableVisualChildren(this DependencyObject obj)
+        {
+            return obj.EnumerableVisualChildren(true);
+        }
+
+        public static IEnumerable<DependencyObject> EnumerableVisualChildren(this DependencyObject obj, bool recursive)
+        {
+            return obj.EnumerableVisualChildren(recursive, true);
+        }
+
+        public static IEnumerable<DependencyObject> EnumerableVisualChildren(this DependencyObject obj, bool recursive, bool sameLevelFirst)
+        {
+            if (obj == null)
+                yield break;
+
+            if (sameLevelFirst)
+            {
+                int count = VisualTreeHelper.GetChildrenCount(obj);
+                List<DependencyObject> list = new List<DependencyObject>(count);
+                for (int i = 0; i < count; i++)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(obj, i);
+                    if (child == null)
+                        continue;
+
+                    yield return child;
+                    if (recursive)
+                    {
+                        list.Add(child);
+                    }
+                }
+
+                foreach (var child in list)
+                {
+                    foreach (DependencyObject grandChild in child.EnumerableVisualChildren(recursive, sameLevelFirst))
+                    {
+                        yield return grandChild;
+                    }
+                }
+            }
+            else
+            {
+                int count = VisualTreeHelper.GetChildrenCount(obj);
+                for (int i = 0; i < count; i++)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(obj, i);
+                    if (child == null)
+                        continue;
+
+                    yield return child;
+                    if (recursive)
+                    {
+                        foreach (var dp in child.EnumerableVisualChildren(recursive, sameLevelFirst))
+                        {
+                            yield return dp;
+                        }
+                    }
+                }
+            }
+        }
+
+        public static T FindVisualChild<T>(this DependencyObject obj, string name) where T : FrameworkElement
+        {
+            foreach (T item in obj.EnumerableVisualChildren(true, true).OfType<T>())
+            {
+                if (item.Name == name)
+                    return item;
+            }
+            return null;
+        }
+
+        public static T GetVisualParent<T>(this DependencyObject obj) where T : DependencyObject
+        {
+            if (obj == null)
+                return null;
+
+            DependencyObject parent = VisualTreeHelper.GetParent(obj);
+            if (parent == null)
+                return null;
+
+            if (typeof(T).IsAssignableFrom(parent.GetType()))
+                return (T)parent;
+
+            return parent.GetVisualParent<T>();
+        }
+
+        public static void RaiseMenuItemClickOnKeyGesture(this ItemsControl control, KeyEventArgs args)
+        {
+            RaiseMenuItemClickOnKeyGesture(control, args, true);
+        }
+
+        public static void RaiseMenuItemClickOnKeyGesture(this ItemsControl control, KeyEventArgs args, bool throwOnError)
+        {
+            if (args == null)
+                throw new ArgumentNullException("e");
+
+            if (control == null)
+                return;
+
+            KeyGestureConverter kgc = new KeyGestureConverter();
+            foreach (var item in control.Items.OfType<MenuItem>())
+            {
+                if (!string.IsNullOrWhiteSpace(item.InputGestureText))
+                {
+                    KeyGesture gesture = null;
+                    if (throwOnError)
+                    {
+                        gesture = kgc.ConvertFrom(item.InputGestureText) as KeyGesture;
+                    }
+                    else
+                    {
+                        try
+                        {
+                            gesture = kgc.ConvertFrom(item.InputGestureText) as KeyGesture;
+                        }
+                        catch
+                        {
+                        }
+                    }
+
+                    if (gesture != null && gesture.Matches(null, args))
+                    {
+                        //System.Diagnostics.Trace.WriteLine("MATCH item:" + item + ", key:" + e.Key + " Keyboard.Modifiers:" + Keyboard.Modifiers);
+                        item.RaiseEvent(new RoutedEventArgs(MenuItem.ClickEvent));
+                        args.Handled = true;
+                        return;
+                    }
+                }
+
+                RaiseMenuItemClickOnKeyGesture(item, args, throwOnError);
+                if (args.Handled)
+                    return;
+            }
+        }
+    }
+}
